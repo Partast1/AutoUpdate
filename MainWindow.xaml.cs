ee6a8f37aa83539602a4e2bb64ba12dc2802c1a8
Anton John Erik Sundström <sundstrom85@gmail.com>
2020-06-23 10:56:33 +0200

    Added classes and methods from local project

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,48 @@ namespace AutoUpdate
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void btnGetVersion_Click(object sender, RoutedEventArgs e)
+        {
+            //Get local project's current version
+            string currentVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            MessageBox.Show("Current version is: " + currentVersion);
+        }
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            //Downloads a file from the server that is used to determine what files to update
+            string sourcePath = System.IO.File.ReadAllText(@".\ServerPath.txt");
+            string targetPath = @".\serverXML.txt";
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(sourcePath);
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+
+            //request.Credentials = new NetworkCredential("anonymous", "janeDoe@contoso.com");
+
+            using (Stream ftpStream = request.GetResponse().GetResponseStream())
+            {
+                using (Stream fileStream = File.Create(targetPath))
+                {
+                    ftpStream.CopyTo(fileStream);
+                }
+            }
+            MessageBox.Show("File downloaded!");
+        }
+
+        //Creates an xml file containing paths to all files in the project
+        private void btnCreateXML_Click(object sender, RoutedEventArgs e)
+        {
+            //@".\"
+            string path = @"C:\Users\anto6789\Desktop\SKP\AutoUpdate\";
+            DirectoryInfo rootPath = new DirectoryInfo(path);
+            int prePathLength = rootPath.FullName.ToString().Length - rootPath.Name.Length - 1;
+
+            XmlHandler xmlObj = new XmlHandler(rootPath, prePathLength);
+            xmlObj.XmlDoc.Save(@".\clientXML.xml");
+            MessageBox.Show("XML file created!");
+        }
+        private void btnCompareXML_Click(object sender, RoutedEventArgs e)
+        {
+            //Compares two xml files and finds the differences between them
         }
     }
 }
---
+++ XmlHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
+
+namespace AutoUpdate
+{
+    class XmlHandler
+    {
+        public XDocument XmlDoc { get; set; }
+        public XmlHandler(DirectoryInfo dir, int pathToRemove)
+        {
+            XmlDoc = CreateXml(dir, pathToRemove);
+        }
+        private XDocument CreateXml(DirectoryInfo dir, int pathToRemove)
+        {
+            return new XDocument(FillXML(dir, pathToRemove));
+        }
+        private XElement FillXML(DirectoryInfo dir, int pathToRemove)
+        {
+            XElement xmlInfo = new XElement("folder", new XAttribute("path", dir.FullName.Remove(0, pathToRemove)), new XAttribute("ID", 1));
+
+            foreach (FileInfo file in dir.GetFiles())
+                xmlInfo.Add(new XElement("file", new XAttribute("path", file.FullName.Remove(0, pathToRemove)), new XAttribute("ID", 1)));
+
+            foreach (DirectoryInfo folder in dir.GetDirectories())
+                xmlInfo.Add(FillXML(folder, pathToRemove));
+
+            return xmlInfo;
+        }
+    }
+}
